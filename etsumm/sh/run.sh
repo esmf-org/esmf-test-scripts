fa294cce76894d1646e62523cf15cff95b1818dd
bekozi <benkoziol@gmail.com>
2020-07-16 12:53:28 -0500

    WIP: Added README

--- etsumm/sh/run.sh
+++ etsumm/sh/run.sh
@@ -2,9 +2,11 @@
 set -Eeuxo pipefail
 
 
-export ESMF_DIR=~/l/esmf
-export ESMF_TESTOUTFILE=~/l/notes/scratch/esmf-make-all_tests.out
-export ESMF_TESTTARGET="all_tests"
+: "${ESMF_TESTOUTFILE?Need to set}"
+
+#export ESMF_DIR=~/l/esmf
+#export ESMF_TESTOUTFILE=~/l/notes/scratch/esmf-make-all_tests.out
+#export ESMF_TESTTARGET="all_tests"
 #export ESMF_TESTTARGET="examples"
 #export ESMF_TESTTARGET="system tests;examples;unit tests"
 TEST_TARGET=../src/etsumm/run.py
