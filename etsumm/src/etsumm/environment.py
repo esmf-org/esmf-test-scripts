4b62e9b41cfe379a2210e0c72240fdcfb307b6c5
bekozi <benkoziol@gmail.com>
2020-07-16 14:54:43 -0500

    WIP: Fix for no ESMF_DIR

--- etsumm/src/etsumm/environment.py
+++ etsumm/src/etsumm/environment.py
@@ -4,13 +4,16 @@ import os
 class Environment(object):
 
     def __init__(self):
-        self.ESMF_DIR = self.format_path(os.environ['ESMF_DIR'])
+        self.ESMF_DIR = self.format_path(os.environ.get('ESMF_DIR'))
         self.ESMF_TESTOUTFILE = self.format_path(os.environ['ESMF_TESTOUTFILE'])
         self.ESMF_TESTTARGET = os.environ.get('ESMF_TESTTARGET', 'all_tests')
 
     @staticmethod
     def format_path(path):
-        return os.path.realpath(os.path.expanduser(path.strip()))
+        if path is not None:
+            return os.path.realpath(os.path.expanduser(path.strip()))
+        else:
+            return None
 
     def validate(self):
         assert os.path.exists(self.ESMF_TESTOUTFILE)
