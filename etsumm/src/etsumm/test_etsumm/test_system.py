4c5c93aa32970884c6d70075196ac4d48df42cb2
bekozi <benkoziol@gmail.com>
2020-07-21 11:11:27 -0500

    WIP: Initial database model with json schema validation

--- etsumm/src/etsumm/test_etsumm/test_system.py
+++ etsumm/src/etsumm/test_etsumm/test_system.py
@@ -1,8 +1,12 @@
 import os
 import subprocess
+import tempfile
 import unittest
 import zipfile
 
+from jsonschema import ValidationError
+
+from etsumm.db.core import initdb, Result
 from etsumm.environment import env
 import etsumm.run
 
@@ -22,7 +26,7 @@ class Test(unittest.TestCase):
         with zipfile.ZipFile(path, 'r') as zip_ref:
             zip_ref.extractall(self.path_bin)
 
-    def test(self):
+    def test_xml_creation(self):
         outfile = os.path.join(self.path_bin, 'esmf-make-all_tests.out')
         env.ESMF_TESTOUTFILE = outfile
         target = os.path.realpath(etsumm.run.__file__)
@@ -30,6 +34,19 @@ class Test(unittest.TestCase):
         if os.path.exists(xmlout):
             os.remove(xmlout)
         try:
-            subprocess.check_call(['pytest', '--junitxml={}'.format(xmlout), target])
+            subprocess.call(['pytest', '--junitxml={}'.format(xmlout), target])
         except:
             self.assertTrue(os.path.exists(xmlout))
+
+    def test_db(self):
+        path = os.path.join(tempfile.gettempdir(), 'test-etsumm-db.sqlite')
+        if os.path.exists(path):
+            os.remove(path)
+        initdb(path, create=True)
+        self.assertTrue(os.path.exists(path))
+        os.remove(path)
+
+        path = os.path.join('tmp', 'should', 'not', 'exist')
+        result_json = os.path.join(self.path_bin, "result.json")
+        with self.assertRaises(ValidationError):
+            result = Result.create(result_json, path)
