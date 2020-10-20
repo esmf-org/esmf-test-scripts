793deb4acfda16e0678673f353b4d50925b4fa95
Himanshu Pillai <himanshupillai@gmail.com>
2020-10-20 10:28:30 -0600

    Updated comm variable

--- etsumm/src/etsumm/cases.py
+++ etsumm/src/etsumm/cases.py
@@ -17,7 +17,7 @@ class TestContainer(unittest.TestCase):
                 test_name += ' {}={}'.format(h, parser.config[h])
             # Path to the test artifacts
             format_values = {h: parser.config[h] for h in env.CONSTANTS['hierarchy']}
-            print("value1:", format_values[5])
+            #print("value1:", format_values[5])
             print("valuex:",format_values["comm"])
             #if(comm=="mpiuni"):
             #    uri = "https://github.com/esmf-org/esmf-test-artifacts/tree/master/{branch}/{platform}/{compiler}/{compiler_version}/{optimization}/{comm}".format(**format_values)
