0d208ac6f14f5545c23268e4b468aa87e2a72f99
Mark Allen Potts <mpotts@koehr04.ib0.icexa.navydsrc.hpc.mil>
2021-02-04 20:57:57 +0000

    update for build-intel_2020.2_intelmpi_O.bat. on koehr

--- get-results.py
+++ get-results.py
@@ -25,11 +25,11 @@ def main(argv):
       
       oe_filelist = glob.glob('{}/{}/*{}*'.format(script_dir,directory,jobid))
       for cfile in oe_filelist:
-        nfile = os.path.basename(re.sub('_{}'.format(jobid), '', cfile))
+        nfile = os.path.basename(re.sub('.{}'.format(jobid), '', cfile))
         cp_cmd = "cp {} {}/{}/{} >& cp_{}".format(cfile,root_path,machine,nfile,jobid)
         print("cp_cmd is {}".format(cp_cmd))
         os.system(cp_cmd)
-      file_root = os.path.basename(re.sub('_{}'.format(jobid), '',oe_filelist[0])
+      file_root = os.path.basename(re.sub('.{}'.format(jobid), '',oe_filelist[0]))
       git_cmd = "cd {};git pull -X theirs;git add {};git commit -a -m\'update for {} on {}\';git push origin python".format(root_path,machine,nfile,machine)
       os.system(git_cmd)
       break
