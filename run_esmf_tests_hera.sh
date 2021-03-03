d47ba87ab1fd111b859f92d081657f344f8cf613
Mark.Potts <Mark.Potts@noaa.gov>
2021-03-03 16:41:02 -0500

    updating run scripts

--- run_esmf_tests_hera.sh
+++ run_esmf_tests_hera.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -l
 
+cd /scratch1/NCEPDEV/da/Mark.Potts/sandbox/esmf-test-scripts
+git remote update
+git pull -X theirs --no-edit 
 module load intelpython
 cd /scratch1/NCEPDEV/da/Mark.Potts/sandbox
 python3 ./esmf-test-scripts/build-test.py ./esmf-test-scripts/hera.yaml $PWD/esmf-test-artifacts
