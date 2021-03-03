1f8a23babe3a637208c4f3318a263be1fda07a9d
Mark.Potts <Mark.Potts@noaa.gov>
2021-03-03 16:26:59 -0500

    now pulling latest version before running

--- run_esmf_tests_discover.sh
+++ run_esmf_tests_discover.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -l
 
 module load python/GEOSpyD/Ana2019.10_py3.7 
+cd /discover/nobackup/mapotts1/esmf-test-scripts
+git remote update
+git pull -X theirs python --no-edit
 cd /discover/nobackup/projects/sbu/mpotts/esmf
 python $NS/esmf-test-scripts/build-test.py $NS/esmf-test-scripts/discover.yaml /discover/nobackup/projects/sbu/mpotts/esmf/esmf-test-artifacts
 
