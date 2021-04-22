671d18b58880629453f2f9c74bdff0d6519cf22c
Mark Allen Potts <mpotts@onyx.erdc.hpc.mil>
2021-04-22 13:28:45 -0500

    updated onyx yaml

--- runscripts/run_esmf_tests_onyx.sh
+++ runscripts/run_esmf_tests_onyx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 
-cd /p/work1/mpotts/esmf-test-scripts
+cd /p/work/mpotts/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit origin
 module load cseinit cse/anaconda3/latest 
-cd /p/work1/mpotts     
+cd /p/work/mpotts     
 python3 ./esmf-test-scripts/python_scripts/test_esmf.py -y  ./esmf-test-scripts/config/onyx.yaml -a $PWD/esmf-test-artifacts
 
 
