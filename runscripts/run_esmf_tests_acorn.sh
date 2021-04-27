f388c2b505e1b64570eb03f3bc4e4a9ce05c414a
Mark A Potts <Mark.Potts@noaa.gov>
2021-04-27 15:06:50 +0000

    updates for acorn

--- runscripts/run_esmf_tests_acorn.sh
+++ runscripts/run_esmf_tests_acorn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -l
-
+export NS=/lfs/h1/emc/nceplibs/noscrub/Mark.Potts
+export ptmp=/lfs/h1/emc/ptmp/Mark.Potts
 cd $NS/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit origin
