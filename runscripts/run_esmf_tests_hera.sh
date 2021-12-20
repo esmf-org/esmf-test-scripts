b4126cacc8579de0097359d420a6dc5d6b9b54e1
Ryan Long <ryan.long@noaa.gov>
2021-12-19 21:37:18 -0700

    Revert "Revert "updated run script for role account""
    This reverts commit eeda989da8bf55ccdcc9c334ef03eac884f56e19.

--- runscripts/run_esmf_tests_hera.sh
+++ runscripts/run_esmf_tests_hera.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 
-cd /scratch1/NCEPDEV/da/Mark.Potts/sandbox/esmf-test-scripts
+cd /scratch1/NCEPDEV/nems/role.esmfmaint/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
-export NS=/scratch1/NCEPDEV/da/Mark.Potts
+export NS=/scratch1/NCEPDEV/nems/role.esmfmaint
 module load intelpython
-cd /scratch1/NCEPDEV/stmp2/Mark.Potts
-python3 $NS/sandbox/esmf-test-scripts/python_scripts/test_esmf.py -y  $NS/sandbox/esmf-test-scripts/config/hera.yaml -a /scratch1/NCEPDEV/stmp2/Mark.Potts/esmf-test-artifacts >& $NS/sandbox/esmf-test-scripts/hera.log &
+cd /scratch1/NCEPDEV/stmp2/role.esmfmaint
+python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -y  $NS/esmf-test-scripts/config/hera.yaml -a /scratch1/NCEPDEV/stmp2/role.esmfmaint/esmf-test-artifacts >& $NS/esmf-test-scripts/hera.log &
 
 
