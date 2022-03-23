1e9b244b0f38c4e2409308722dfa6143b667fe5b
Ryan Long <ryanlong1004@gmail.com>
2022-03-22 20:11:52 -0400

    more debug

--- python_scripts/pbs.py
+++ python_scripts/pbs.py
@@ -57,10 +57,11 @@ class PBS(Scheduler):
             return True
         queue_query = f"qstat -H {jobid} | tail -n 1 | awk -F ' +' '{{print $10}}'"
         try:
-            return (
-                    subprocess.check_output(queue_query, shell=True).strip().decode("utf-8")
-                    == "F"
-            )
+            result = subprocess.check_output(queue_query, shell=True).strip().decode("utf-8")
+            logging.debug("job status is [%s]", result)
+            return result.lower().strip() == 'f'
+
+
         except subprocess.CalledProcessError:
             pass
         finally:
