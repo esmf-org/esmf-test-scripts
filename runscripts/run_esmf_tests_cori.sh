7b215f4113181050017d6742435852bd048179f3
Rocky Dunlap <dunlap@ucar.edu>
2022-05-16 14:06:21 -0700

    Update Cori config

--- runscripts/run_esmf_tests_cori.sh
+++ runscripts/run_esmf_tests_cori.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
-cd /global/homes/r/rsdunlap/esmf-testing/esmf-test-scripts
+cd $SCRATCH/esmf-testing/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit origin 
-cd /global/homes/r/rsdunlap/esmf-testing
-python3 ./esmf-test-scripts/python_scripts/test_esmf.py -y  ./esmf-test-scripts/config/cori.yaml -a /global/homes/r/rsdunlap/esmf-testing/esmf-test-artifacts >& /global/homes/r/rsdunlap/esmf-testing/cori.log
+cd $SCRATCH/esmf-testing
+python3 ./esmf-test-scripts/python_scripts/test_esmf.py -y  ./esmf-test-scripts/config/cori.yaml -a $SCRATCH/esmf-testing/esmf-test-artifacts >& $SCRATCH/esmf-testing/cori.log
