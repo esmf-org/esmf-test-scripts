6e28ccc70bce85e719954c46bea3602afcd70723
Bill Sacks <sacks@ucar.edu>
2022-05-26 20:42:16 -0600

    izumi run script: generalize user, output to file

--- runscripts/run_esmf_tests_izumi.sh
+++ runscripts/run_esmf_tests_izumi.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -l
 
-cd /scratch/cluster/dunlap/esmf-testing/esmf-test-scripts
+cd /scratch/cluster/${USER}/esmf-testing/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit origin 
-cd /scratch/cluster/dunlap/esmf-testing
-python3 ./esmf-test-scripts/python_scripts/test_esmf.py -y  ./esmf-test-scripts/config/izumi.yaml -a /scratch/cluster/dunlap/esmf-testing/esmf-test-artifacts
-
-#  /scratch/cluster/dunlap/esmf-testing/izumi.log
+cd /scratch/cluster/${USER}/esmf-testing
+python3 ./esmf-test-scripts/python_scripts/test_esmf.py -y  ./esmf-test-scripts/config/izumi.yaml -a /scratch/cluster/${USER}/esmf-testing/esmf-test-artifacts >& /scratch/cluster/${USER}/esmf-testing/test_esmf_izumi.log
