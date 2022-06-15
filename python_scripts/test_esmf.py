99766ea74e470329a4065af84217e8ea866c4c03
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-06-14 21:06:54 -0600

    Working on allowing reclone to work if machine branch already exists in the remote esmf-test-artifacts or needs to be created.  Both options should function correctly.

--- python_scripts/test_esmf.py
+++ python_scripts/test_esmf.py
@@ -33,8 +33,8 @@ class ESMFTest:
       os.system("git clone {}".format(REPO_ESMF_TEST_ARTIFACTS))
       os.chdir("esmf-test-artifacts")
       os.system("git fetch origin")
-      os.system("git checkout -b {}".format(self.machine_name))
-      os.system("git branch --set-upstream-to origin/{}".format(self.machine_name))
+      os.system("git checkout -B {}".format(self.machine_name))
+      os.system("git push --set-upstream origin {}".format(self.machine_name))
       os.chdir("..")
     if(self.scheduler_type == "slurm"):
       self.scheduler=slurm("slurm")
