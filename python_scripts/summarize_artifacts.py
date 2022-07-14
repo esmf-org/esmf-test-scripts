ab5d4fc58891cc2c0b5674ba9c32be0583139457
Rocky Dunlap <dunlap@ucar.edu>
2022-07-14 01:20:02 -0600

    WIP:  Partially refactored version (26)

--- python_scripts/summarize_artifacts.py
+++ python_scripts/summarize_artifacts.py
@@ -12,7 +12,9 @@ import sqlite3
 
 ComboRecord = namedtuple('ComboRecord', 'id, machine, compiler, compiler_ver, bopt, mpi, mpi_ver, netcdf')
 ResultRecord = namedtuple('ResultRecord',
-                          'hash, collect_ts, combination_id, esmf_hash, status, build, build_ts, unit_pass, unit_fail')
+                          'hash, collect_ts, combination_id, esmf_branch, esmf_hash, status, build, build_ts, '
+                          'unit_pass, unit_fail, system_pass, system_fail, example_pass, example_fail, nuopc_pass, '
+                          'nuopc_fail')
 
 
 def _init_database():
@@ -40,12 +42,19 @@ def _init_database():
             hash text NOT NULL PRIMARY KEY,
             collect_ts datetime NOT NULL,            
             combination_id integer NOT NULL,
+            esmf_branch text NOT NULL,
             esmf_hash text NOT NULL,
             status text NOT NULL,
             build text,
             build_ts datatime,
             unit_pass integer,
             unit_fail integer,
+            system_pass integer,
+            system_fail integer,
+            example_pass integer,
+            example_fail integer,
+            nuopc_pass integer,
+            nuopc_fail integer,
             FOREIGN KEY (combination_id) REFERENCES combination (id),
             UNIQUE (collect_ts, combination_id, esmf_hash)
         );
@@ -82,8 +91,10 @@ def _insert_result(result: ResultRecord):
     cur = dbconn.cursor()
     cur.execute(
         """
-        INSERT INTO result (hash, collect_ts, combination_id, esmf_hash, status, build, build_ts, unit_pass, unit_fail)
-        VALUES (?,?,?,?,?,?,?,?,?)
+        INSERT INTO result (hash, collect_ts, combination_id, esmf_branch, esmf_hash, status, build, build_ts, 
+                            unit_pass, unit_fail, system_pass, system_fail, example_pass, example_fail, nuopc_pass,
+                            nuopc_fail)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
         """,
         result)
     dbconn.commit()
@@ -98,6 +109,68 @@ def _retrieve_combo(combo: ComboRecord):
         WHERE machine=? AND compiler=? AND compiler_ver=? AND mpi=? AND mpi_ver=? AND netcdf=?
         """)
     logging.debug(f"all: {cur.fetchall()}")
+
+
+def _retrieve_tested_hashes():
+    cur = dbconn.cursor()
+    cur.execute(
+        """
+        SELECT 
+            esmf_branch, esmf_hash, 
+            COUNT(DISTINCT combination_id) AS combo_count, 
+            replace(group_concat(DISTINCT machine || '/' || compiler), ',', ', ') AS machines
+        FROM result INNER JOIN combination ON result.combination_id = combination.id
+        GROUP BY esmf_branch, esmf_hash
+        ORDER BY esmf_hash DESC
+        """)
+    return cur.fetchall()
+
+
+def _retrieve_summary_for_esmf_hash(esmf_hash):
+    cur = dbconn.cursor()
+    cur.execute(
+        """
+        SELECT machine, compiler, compiler_ver, mpi, mpi_ver, bopt, netcdf, 
+            STRFTIME('%m-%d %H:%M', collect_ts) as collect_ts, 
+            STRFTIME('%m-%d %H:%M', build_ts) as build_ts,
+            build, unit_pass, unit_fail, system_pass, system_fail, example_pass, example_fail, nuopc_pass, 
+            nuopc_fail, esmf_hash, esmf_branch
+        FROM result INNER JOIN combination ON result.combination_id = combination.id
+        WHERE esmf_hash = ?
+        GROUP BY build_ts
+        ORDER BY machine, compiler, compiler_ver, mpi, mpi_ver
+        """, (esmf_hash,))
+    return cur.fetchall()
+
+
+def _print_summary_for_esmf_hash(esmf_hash):
+    _cfmt = "{:<14} {:<18} {:<18} {:<5} {:<12} {:<14} {:<8} {:<10} {:<10} {:<10} {:<10}"
+    print(f"\nESMF HASH: {esmf_hash}")
+    print("=" * 140)
+    print(_cfmt.format('Machine', 'Compiler', 'MPI', 'BOPT', 'NetCDF', 'Collected', 'Build', 'UTests', 'STests', 'ETests', 'NUOPC'))
+    print("=" * 140)
+    for i, r in enumerate(_retrieve_summary_for_esmf_hash(esmf_hash), start=1):
+        print(_cfmt.format(r["machine"],
+                           r["compiler"]+"/"+r["compiler_ver"],
+                           r["mpi"]+"/"+r["mpi_ver"],
+                           r["bopt"],
+                           r["netcdf"],
+                           r["collect_ts"],
+                           r["build"],
+                           str(r["unit_pass"]) + "/" + str(r["unit_fail"]),
+                           str(r["system_pass"]) + "/" + str(r["system_fail"]),
+                           str(r["example_pass"]) + "/" + str(r["example_fail"]),
+                           str(r["nuopc_pass"]) + "/" + str(r["nuopc_fail"])))
+
+
+def _print_tested_hashes():
+    print("\nHashes of ESMF in the test artifacts database:")
+    print("=" * 120)
+    print("{:<4} {:<45} {:<8} {}".format('No.', 'Hash (Branch)', 'Combos', 'Machines'))
+    print("=" * 120)
+    for i, h in enumerate(_retrieve_tested_hashes(), start=1):
+        _hb = f"{h[1]} ({h[0]})"
+        print("{:<4} {:<45} {:<8} {}".format(i, _hb, h[2], h[3]))
 
 
 def _get_machine_list(repo):
@@ -117,18 +190,18 @@ def _get_commits(repo, machine_branch):
     cmd.runcmd(f"git checkout {machine_branch}")
     cmd.runcmd(f"git fetch origin {machine_branch}")
     cmd.runcmd(f"git reset --hard origin/{machine_branch}")
-    _log_out = cmd.runcmd(f"git log --format='%H %aI %s' -n 10")
+    _log_out = cmd.runcmd(f"git log --format='%H %aI %s' -n 100")
     _log_list = _log_out.splitlines()
 
     for _log_entry in _log_list:
         # logging.debug(_log_entry)
         _commit_dict = {"hash": _log_entry[0:40],  # hash of artifacts commit
-                        "ts": _log_entry[41:66],   # timestamp of artifacts commit
+                        "ts": _log_entry[41:66],  # timestamp of artifacts commit
                         "action": _extract(r"action=(\S+)", _log_entry),
                         "dir": _extract(r"dir=(\S+)", _log_entry),
                         # TODO: change hash to esmf_hash in commit message
-                        "esmf_hash": _extract(r"hash=(\S+)", _log_entry), # hash of ESMF tested
-                        "esmf_branch": _extract(r"branch=(\S+)", _log_entry) # branch of ESMF tested
+                        "esmf_hash": _extract(r"hash=(\S+)", _log_entry),  # hash of ESMF tested
+                        "esmf_branch": _extract(r"branch=(\S+)", _log_entry)  # branch of ESMF tested
                         }
         # logging.debug(f"_commit_dict = {_commit_dict}")
 
@@ -136,12 +209,15 @@ def _get_commits(repo, machine_branch):
             _collect_summary_stats(_commit_dict, machine_branch)
 
 
-def _extract(regex, string):
+def _extract(regex, string, default=None):
     _match = re.search(regex, string)
     if _match is not None:
-        return _match.group(1)
+        if len(_match.groups()) == 1:
+            return _match.group(1)
+        else:
+            return _match.groups()
     else:
-        return None
+        return default
 
 
 def _collect_summary_stats(commit, machine_branch):
@@ -158,17 +234,21 @@ def _collect_summary_stats(commit, machine_branch):
         _combo = ComboRecord._make((None, machine_branch, _m["compiler"], _m["compiler_ver"],
                                     _m["bopt"], _m["mpi"], _m["mpi_ver"], "netcdf"))
         _combo_id = _insert_combo(_combo)
-        logging.debug(f"\tcombo = {_combo}\t id = {_combo_id}")
 
         _summary_text = cmd.runcmd(f"cat {_summary_file}")
         # logging.debug(_summary_text)
         _esmf_hash = _extract(r"ESMF hash: (\S+)", _summary_text)
         _build_ts = _extract(r"Build timestamp from esmf\.mk:\s+(.+)\n", _summary_text)
+        _build_pass = _extract(r"build:\s+(\S+)", _summary_text)
+        _unit = _extract(r"unit tests:\s+PASS\s+(\S+)\s+FAIL\s+(\S+)", _summary_text, (None, None))
+        _system = _extract(r"system tests:\s+PASS\s+(\S+)\s+FAIL\s+(\S+)", _summary_text, (None, None))
+        _example = _extract(r"example tests:\s+PASS\s+(\S+)\s+FAIL\s+(\S+)", _summary_text, (None, None))
+        _nuopc = _extract(r"nuopc tests:\s+PASS\s+(\S+)\s+FAIL\s+(\S+)", _summary_text, (None, None))
 
-
-        #  hash, collect_ts, combination_id, esmf_hash, status, build, build_ts, unit_pass, unit_fail
-        _result = ResultRecord._make((_hash, commit["ts"], _combo_id, _esmf_hash, 'S', 'PASS', _build_ts, 100, 200))
-        logging.debug(f"RESULT: {_result}")
+        _result = ResultRecord._make((_hash, commit["ts"], _combo_id, commit["esmf_branch"],
+                                      _esmf_hash, 'S', _build_pass, _build_ts, _unit[0], _unit[1],
+                                      _system[0], _system[1], _example[0], _example[1], _nuopc[0], _nuopc[1]))
+        # logging.debug(f"RESULT: {_result}")
         _insert_result(_result)
 
 
@@ -195,17 +275,17 @@ if __name__ == "__main__":
         exit(1)
 
     dbconn = sqlite3.connect(f"{os.path.join(_db_path, 'esmf_test_summary.sqlite3')}")
+    dbconn.row_factory = sqlite3.Row
 
-    _init_database()
-    # _insert_combo()
-
-    # _retrieve_data()
+    # _init_database()
 
     # _mach_list = _get_machine_list(_repo_path)
-    # logging.debug(_mach_list)
-
     # for _m in _mach_list:
-    _get_commits(_repo_path, "cheyenne")
-    _get_commits(_repo_path, "catania")
+    # _get_commits(_repo_path, "cheyenne")
+    # _get_commits(_repo_path, "catania")
+
+    _print_tested_hashes()
+
+    _print_summary_for_esmf_hash('v8.4.0b03-6-gd94fc0f')
 
     dbconn.close()
