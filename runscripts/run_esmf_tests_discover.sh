9b5be1654a03e2098d283da73705dc88b7287fe1
Mark.Potts <Mark.Potts@noaa.gov>
2022-07-22 14:24:02 -0400

    updated yaml and run script for discover

--- runscripts/run_esmf_tests_discover.sh
+++ runscripts/run_esmf_tests_discover.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -l
 
 module load python/GEOSpyD/Ana2019.10_py3.7 git/2.30.0
-cd /discover/nobackup/mapotts1/esmf-test-scripts
+cd /discover/nobackup/projects/sbu/mpotts/esmf-test/esmf-test-scripts
 git remote update
 git pull -X theirs origin --no-edit
-cd /discover/nobackup/projects/sbu/mpotts/esmf
-python /discover/nobackup/mapotts1/esmf-test-scripts/python_scripts/test_esmf.py -y  /discover/nobackup/mapotts1/esmf-test-scripts/config/discover.yaml -a /discover/nobackup/projects/sbu/mpotts/esmf/esmf-test-artifacts
-
+/discover/nobackup/projects/sbu/mpotts/esmf-test/esmf-test-scripts/python_scripts/test_esmf.py -r /discover/nobackup/projects/sbu/mpotts/esmf-test -m discover 
 
