bc0d875ede05b6b60576ab353ab8bcf5ead87bca
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-08-24 11:16:19 -0600

    Update cheyenne run script

--- runscripts/run_esmf_tests_cheyenne.sh
+++ runscripts/run_esmf_tests_cheyenne.sh
@@ -3,6 +3,6 @@
 cd /glade/scratch/dunlap/esmf-testing/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit origin
-module load python/3.6.8
+module load python/3.7.9
 cd /glade/scratch/dunlap/esmf-testing
-python3 ./esmf-test-scripts/python_scripts/test_esmf.py -y  ./esmf-test-scripts/config/cheyenne.yaml -a /glade/scratch/dunlap/esmf-testing/esmf-test-artifacts >& /glade/scratch/dunlap/esmf-testing/cheyenne.log
+python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m cheyenne -r /glade/scratch/dunlap/esmf-testing --throttle 4 >& /glade/scratch/dunlap/esmf-testing/cheyenne.log
