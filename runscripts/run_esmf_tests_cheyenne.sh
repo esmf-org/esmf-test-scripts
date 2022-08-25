ac36a861110050e696826e4706c74762fc755110
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-08-25 16:54:02 -0600

    Increase throttle rate on Cheyenne

--- runscripts/run_esmf_tests_cheyenne.sh
+++ runscripts/run_esmf_tests_cheyenne.sh
@@ -5,4 +5,4 @@ git remote update
 git pull -X theirs --no-edit origin
 module load python/3.7.9
 cd /glade/scratch/dunlap/esmf-testing
-python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m cheyenne -r /glade/scratch/dunlap/esmf-testing --throttle 4 >& /glade/scratch/dunlap/esmf-testing/cheyenne.log
+python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m cheyenne -r /glade/scratch/dunlap/esmf-testing --throttle 8 >& /glade/scratch/dunlap/esmf-testing/cheyenne.log
