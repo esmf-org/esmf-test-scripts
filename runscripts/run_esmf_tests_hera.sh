0ffaceaffad448a33715c07709aa95986b40f843
Rocky Dunlap <Rocky.Dunlap@noaa.gov>
2022-08-26 19:48:31 +0000

    Update hera runscript

--- runscripts/run_esmf_tests_hera.sh
+++ runscripts/run_esmf_tests_hera.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -l
 
-cd /scratch1/NCEPDEV/nems/role.esmfmaint/esmf-test-scripts
+cd /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
-export NS=/scratch1/NCEPDEV/nems/role.esmfmaint
+export NS=/scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing
 module load intelpython
-cd /scratch1/NCEPDEV/stmp2/role.esmfmaint
-python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -y  $NS/esmf-test-scripts/config/hera.yaml -a /scratch1/NCEPDEV/stmp2/role.esmfmaint/esmf-test-artifacts >& $NS/esmf-test-scripts/hera.log &
-
+$NS/esmf-test-scripts/python_scripts/test_esmf.py -m hera -r /scratch1/NCEPDEV/stmp2/Rocky.Dunlap/esmf-testing >& $NS/hera.log &
 
