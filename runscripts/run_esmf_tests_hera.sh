3d7428cad922a5f91dcb2d2e172e21421f4e0b73
Gerhard Theurich <theurich@sourcespring.net>
2022-10-04 11:36:35 -0700

    Simplify.

--- runscripts/run_esmf_tests_hera.sh
+++ runscripts/run_esmf_tests_hera.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 
-cd /scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing/esmf-test-scripts
+export NS=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing
+cd $NS/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
-export NS=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing
 module load intelpython
 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m hera -r $NS >& $NS/hera.log &
 
