8b2521239f13088172ee5d96bfa3ea3822f7a995
Gerhard Theurich <theurich@sourcespring.net>
2022-10-04 21:26:31 -0700

    Switch nightly testing on Cori to Gerhard.

--- runscripts/run_esmf_tests_cori.sh
+++ runscripts/run_esmf_tests_cori.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -l
 
-cd $SCRATCH/esmf-testing/esmf-test-scripts
+eval `ssh-agent`
+ssh-add /global/homes/t/theurich/.ssh/id_ed25519
+
+export NS=/global/cscratch1/sd/theurich/ESMF-Nightly-Testing
+cd $NS/esmf-test-scripts
 git remote update
-git pull -X theirs --no-edit origin 
-cd $SCRATCH/esmf-testing
-./esmf-test-scripts/python_scripts/test_esmf.py -m cori -r $SCRATCH/esmf-testing >& $SCRATCH/esmf-testing/cori.log &
+git pull -X theirs --no-edit origin
+$NS/esmf-test-scripts/python_scripts/test_esmf.py -m cori -r $NS >& $NS/cori.log &
+
+kill $SSH_AGENT_PID
+
