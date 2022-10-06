a7baa25cb13edfd7116a520adf9597f3f1589c61
Bill Sacks <sacks@ucar.edu>
2022-10-05 21:14:26 -0600

    Add test configuration on my Mac, green

---
+++ runscripts/run_esmf_tests_green.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+
+esmftestroot=/Users/sacks/projects/scratch/esmf-testing
+cd ${esmftestroot}/esmf-test-scripts
+git remote update
+git pull -X theirs --no-edit
+cd ${esmftestroot}
+python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m green -r ${esmftestroot} >& ${esmftestroot}/test_esmf_green.log &
