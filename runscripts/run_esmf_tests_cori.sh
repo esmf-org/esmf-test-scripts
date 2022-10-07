f313523bec6e84686a2675ff87878c8e95cb45bd
Gerhard Theurich <theurich@sourcespring.net>
2022-10-07 16:08:36 -0700

    Align with cheyenne, more info from script run under cron.

--- runscripts/run_esmf_tests_cori.sh
+++ runscripts/run_esmf_tests_cori.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -l
 
+date
+
+hostname -f
+
 eval `ssh-agent`
 ssh-add /global/homes/t/theurich/.ssh/id_ed25519
 
@@ -7,7 +11,9 @@ export NS=/global/cscratch1/sd/theurich/ESMF-Nightly-Testing
 cd $NS/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit origin
-$NS/esmf-test-scripts/python_scripts/test_esmf.py -m cori -r $NS >& $NS/cori.log &
+$NS/esmf-test-scripts/python_scripts/test_esmf.py -m cori -r $NS >& $NS/cori.log
 
 #kill $SSH_AGENT_PID
+
+date
 
