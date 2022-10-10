bc84c89abc64a98168e69d2f49477ef46216df16
Gerhard Theurich <theurich@sourcespring.net>
2022-10-10 15:59:47 +0000

    Alignment of run scripts.

--- runscripts/run_esmf_tests_hera.sh
+++ runscripts/run_esmf_tests_hera.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -l
 
+date
+
+hostname -f
+
 export NS=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing
 cd $NS/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
 module load intelpython
-$NS/esmf-test-scripts/python_scripts/test_esmf.py -m hera -r $NS >& $NS/hera.log &
+python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m hera -r $NS >& $NS/hera.log 
+
+date
 
