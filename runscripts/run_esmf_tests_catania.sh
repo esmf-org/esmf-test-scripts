64f44d4f1c2049582677c60b94e1f3040e1684dc
Bill Sacks <sacks@ucar.edu>
2022-10-10 20:47:00 -0600

    Change catania testing to my account

--- runscripts/run_esmf_tests_catania.sh
+++ runscripts/run_esmf_tests_catania.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -l
 
-cd /project/esmf/rocky/esmf-testing/esmf-test-scripts
+cd /project/esmf/sacks/esmf-testing/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
-cd /project/esmf/rocky/esmf-testing
-python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m catania -r /project/esmf/rocky/esmf-testing >& /project/esmf/rocky/esmf-testing/test_esmf_catania.log &
-
+cd /project/esmf/sacks/esmf-testing
+python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m catania -r /project/esmf/sacks/esmf-testing >& /project/esmf/sacks/esmf-testing/test_esmf_catania.log &
