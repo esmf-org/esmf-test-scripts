fb46ac3a96eb15da5d2e09bdbfc812ea671b5432
Bill Sacks <sacks@ucar.edu>
2022-10-14 07:20:42 -0600

    Revert "catania run script: do not background python command"
    This reverts commit 2fd97a91be7ab957d02d8b032e0fd9259bb0f071.
    
    I'm changing to cron, so am going back to backgrounding this job for
    consistency with other scripts.

--- runscripts/run_esmf_tests_catania.sh
+++ runscripts/run_esmf_tests_catania.sh
@@ -4,4 +4,4 @@ cd /project/esmf/sacks/esmf-testing/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
 cd /project/esmf/sacks/esmf-testing
-python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m catania -r /project/esmf/sacks/esmf-testing >& /project/esmf/sacks/esmf-testing/test_esmf_catania.log
+python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m catania -r /project/esmf/sacks/esmf-testing >& /project/esmf/sacks/esmf-testing/test_esmf_catania.log &
