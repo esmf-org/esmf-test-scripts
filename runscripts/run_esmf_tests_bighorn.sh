d676e79f5564a098d0da1f6b7288bbeae5c57fa1
Gerhard Theurich <theurich@sourcespring.net>
2022-12-09 13:18:07 -0700

    Initial run script for Bighorn.

---
+++ runscripts/run_esmf_tests_bighorn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+date
+
+hostname -f
+
+pkill -u theurich ssh-agent
+
+eval `ssh-agent`
+ssh-add /home/theurich/.ssh/id_ed25519
+
+export NS=/project/esmf/theurich/ESMF-Nightly-Testing
+cd $NS/esmf-test-scripts
+git remote update
+git pull -X theirs --no-edit
+module load lang/python/3.7.0
+python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m bighorn -r $NS >& $NS/bighorn.log
+
+date
