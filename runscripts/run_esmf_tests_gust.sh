77804cfa5ffa8cda1003ee29aa4f8f7d8db84653
Gerhard Theurich <theurich@sourcespring.net>
2023-02-02 19:46:45 -0700

    A few corrections after Gust changes.

--- runscripts/run_esmf_tests_gust.sh
+++ runscripts/run_esmf_tests_gust.sh
@@ -8,7 +8,7 @@ export NS=/glade/gust/scratch/theurich/ESMF-Nightly-Testing
 cd $NS/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
-python3.6 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m gust -r $NS >& $NS/gust.log 
+$NS/esmf-test-scripts/python_scripts/test_esmf.py -m gust -r $NS >& $NS/gust.log 
 
 date
 
