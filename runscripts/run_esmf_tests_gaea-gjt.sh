98e99e3921e0722d019310605b0bbe29ab2ac590
Gerhard Theurich <theurich@sourcespring.net>
2023-05-18 18:01:12 -0400

    Customized script that works under Gerhard's account settings.

---
+++ runscripts/run_esmf_tests_gaea-gjt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+date
+
+hostname -f
+
+pkill -u Gerhard.Theurich ssh-agent
+
+eval `ssh-agent -s`
+ssh-add /ncrc/home2/Gerhard.Theurich/.ssh/id_ed25519
+
+export NS=/lustre/f2/dev/ncep/Gerhard.Theurich/ESMF-Nightly-Testing-C3_4
+cd $NS/esmf-test-scripts
+git remote update
+git pull -X theirs --no-edit
+python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m gaea -r $NS >& $NS/gaea.log
+
+date
