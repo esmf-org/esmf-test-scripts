472c0b10b4be6085dd2542760282aaa26b6f777c
Gerhard Theurich <theurich@sourcespring.net>
2023-05-25 04:11:21 +0000

    Change to a python module that works after Hera changes.

--- runscripts/run_esmf_tests_hera.sh
+++ runscripts/run_esmf_tests_hera.sh
@@ -8,7 +8,7 @@ export NS=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing
 cd $NS/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
-module load intelpython
+module load intelpython/2021.3.0
 python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m hera -r $NS >& $NS/hera.log 
 
 date
