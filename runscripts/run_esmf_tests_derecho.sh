972d33f752c1ad5eea4f024ac2f3b8aa342f2a8f
Gerhard Theurich <theurich@sourcespring.net>
2023-06-09 17:28:33 -0600

    Add Derecho.

---
+++ runscripts/run_esmf_tests_derecho.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+date
+
+hostname -f
+
+eval `ssh-agent`
+ssh-add /glade/u/home/theurich/.ssh/id_ed25519
+
+export NS=/glade/derecho/scratch/theurich/ESMF-Nightly-Testing
+cd $NS/esmf-test-scripts
+git remote update
+git pull -X theirs --no-edit origin
+python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m derecho -r $NS --throttle 8 >& $NS/derecho.log
+
+#kill $SSH_AGENT_PID
+
+date
