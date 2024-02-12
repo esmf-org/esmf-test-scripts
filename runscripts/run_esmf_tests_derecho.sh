4fae9ef704b89c522706d2b494325c11701a5326
Bill Sacks <sacks@ucar.edu>
2024-02-12 15:56:44 -0700

    Add esmpy testing on derecho

--- runscripts/run_esmf_tests_derecho.sh
+++ runscripts/run_esmf_tests_derecho.sh
@@ -11,6 +11,11 @@ export NS=/glade/derecho/scratch/theurich/ESMF-Nightly-Testing
 cd $NS/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit origin
+
+# The following is needed for ESMPy testing
+module load conda/latest
+conda activate npl
+
 python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m derecho -r $NS --throttle 8 >& $NS/derecho.log
 
 #kill $SSH_AGENT_PID
