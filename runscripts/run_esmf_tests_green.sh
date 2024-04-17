46c2d76a2afb73537e382f09e7377e57e3d341e9
Bill Sacks <sacks@ucar.edu>
2024-04-17 17:23:02 -0600

    Add ESMPy testing on green
    This leverages the new method to create a conda environment for ESMPy
    testing.

--- runscripts/run_esmf_tests_green.sh
+++ runscripts/run_esmf_tests_green.sh
@@ -4,5 +4,10 @@ esmftestroot=/Users/sacks/projects/scratch/esmf-testing
 cd ${esmftestroot}/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit
+
+# The following is needed for ESMPy testing
+./py_env_creation/py_env_create --path ${esmftestroot} --file environment_python311.yml
+conda activate ${esmftestroot}/esmf-test-scripts-conda-env
+
 cd ${esmftestroot}
 python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m green -r ${esmftestroot} >& ${esmftestroot}/test_esmf_green.log &
