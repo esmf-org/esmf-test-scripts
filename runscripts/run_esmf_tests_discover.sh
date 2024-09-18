58d0416720d4aec6019b9da6d2db407ce960096b
Matthew Thompson <matthew.thompson@nasa.gov>
2024-09-18 12:36:29 -0400

    Updates for discover (mathomp4)

--- runscripts/run_esmf_tests_discover.sh
+++ runscripts/run_esmf_tests_discover.sh
@@ -1,8 +1,18 @@
 #!/bin/bash -l
 
-module load python/GEOSpyD/Ana2019.10_py3.7 git/2.30.0
-cd /discover/nobackup/projects/sbu/mpotts/esmf-test/esmf-test-scripts
+ESMF_TEST_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing
+
+# These are for SLES12
+#module use /discover/swdev/gmao_SIteam/modulefiles-SLES12
+#module load python/GEOSpyD/Min24.4.0-0_py3.11 git/2.40.1
+# These are for SLES15
+module use /discover/swdev/gmao_SIteam/modulefiles-SLES15
+module load python/GEOSpyD/24.3.0-0/3.11 git/2.45.0
+
+cd $ESMF_TEST_ROOT/esmf-test-scripts
+
 git remote update
 git pull -X theirs origin --no-edit
-/discover/nobackup/projects/sbu/mpotts/esmf-test/esmf-test-scripts/python_scripts/test_esmf.py -r /discover/nobackup/projects/sbu/mpotts/esmf-test -m discover 
+
+$ESMF_TEST_ROOT/esmf-test-scripts/python_scripts/test_esmf.py -r $ESMF_TEST_ROOT -m discover 
 
