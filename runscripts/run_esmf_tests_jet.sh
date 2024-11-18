8f9c1fd8abfdf617b4ee4e38f6d9fa98388786ce
Gerhard Theurich <Gerhard Theurich>
2024-11-18 23:49:50 +0000

    Re-activate Jet testing.

--- runscripts/run_esmf_tests_jet.sh
+++ runscripts/run_esmf_tests_jet.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -l
 
-cd /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-test-scripts
+date
+
+hostname -f
+
+export NS=/mnt/lfs5/BMC/esmf/ESMF-Nightly-Testing
+cd $NS/esmf-test-scripts
 git remote update
-git pull -X theirs --no-edit 
-export NS=/lfs4/HFIP/hfv3gfs/Mark.Potts
+git pull -X theirs --no-edit
 module load intelpython
-$NS/esmf-test-scripts/python_scripts/test_esmf.py -r /lfs4/HFIP/hfv3gfs/Mark.Potts/esmf-tests --machine jet 
+python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m jet -r $NS >& $NS/jet.log
 
-
+date
