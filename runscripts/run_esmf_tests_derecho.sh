9b3938463e2572e1717f8c40b2b4e54815c6a559
Bill Sacks <sacks@ucar.edu>
2024-11-26 16:35:23 -0700

    On derecho, put esmf-test-scripts in a non-scrubbed location
    The derecho tests have been failing. It seems like the issue is that
    some files were scrubbed from my esmf-test-scripts directory.

--- runscripts/run_esmf_tests_derecho.sh
+++ runscripts/run_esmf_tests_derecho.sh
@@ -8,11 +8,13 @@ eval `ssh-agent`
 ssh-add /glade/u/home/sacks/.ssh/id_rsa
 
 export NS=/glade/derecho/scratch/sacks/esmf-testing
-cd $NS/esmf-test-scripts
+# Put esmf-test-scripts in a non-scrubbed location:
+export TESTSCRIPTS=/glade/work/sacks/esmf-testing/esmf-test-scripts
+cd $TESTSCRIPTS
 git remote update
 git pull -X theirs --no-edit origin
 
-python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m derecho -r $NS --throttle 8 >& $NS/derecho.log
+python3 $TESTSCRIPTS/python_scripts/test_esmf.py -m derecho -r $NS --throttle 8 >& $NS/derecho.log
 
 #kill $SSH_AGENT_PID
 
