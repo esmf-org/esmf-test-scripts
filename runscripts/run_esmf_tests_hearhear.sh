527736d362f7de12937199663a48b6a1acb7bad8
Bill Sacks <sacks@ucar.edu>
2025-01-06 16:33:59 -0700

    Use caffeinate to make sure hearhear stays awake for testing
    This may not be necessary, but it at least doesn't seem to hurt

--- runscripts/run_esmf_tests_hearhear.sh
+++ runscripts/run_esmf_tests_hearhear.sh
@@ -6,4 +6,7 @@ git remote update
 git pull -X theirs --no-edit
 
 cd ${esmftestroot}
+# Use caffeinate to make sure that the system doesn't sleep for the next 24 hours.
+# caffeinate may not be necessary here, but it at least doesn't hurt.
+caffeinate -t 86400 -i -s &
 python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m hearhear -r ${esmftestroot} >& ${esmftestroot}/test_esmf_hearhear.log &
