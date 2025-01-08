7fc68070c152ce9272701ff5fa7ff23bb596eb6d
Bill Sacks <sacks@ucar.edu>
2025-01-08 07:10:53 -0700

    On hearhear, wrap the python command in an AppleScript
    Wrap the python script in a call from an AppleScript to get the cron
    job to run faster (From
    https://stackoverflow.com/questions/74793041/why-does-selenium-python-script-run-really-slow-in-cron-compared-to-vscode)
    This leads to about a 2.5x speedup in test time

--- runscripts/run_esmf_tests_hearhear.sh
+++ runscripts/run_esmf_tests_hearhear.sh
@@ -6,7 +6,12 @@ git remote update
 git pull -X theirs --no-edit
 
 cd ${esmftestroot}
+
 # Use caffeinate to make sure that the system doesn't sleep for the next 24 hours.
 # caffeinate may not be necessary here, but it at least doesn't hurt.
 caffeinate -t 86400 -i -s &
-python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m hearhear -r ${esmftestroot} >& ${esmftestroot}/test_esmf_hearhear.log &
+
+# Wrap the python script in a call from an AppleScript to get the cron job to run faster
+# (From https://stackoverflow.com/questions/74793041/why-does-selenium-python-script-run-really-slow-in-cron-compared-to-vscode)
+# This leads to about a 2.5x speedup in test time
+osascript -e "tell application \"Terminal\" to do script \"cd ${esmftestroot} && python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m hearhear -r ${esmftestroot} >& ${esmftestroot}/test_esmf_hearhear.log\""
