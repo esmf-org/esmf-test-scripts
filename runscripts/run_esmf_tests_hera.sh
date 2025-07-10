5388e011f6b51ec6ecce9a45304b1fde951d4feb
Gerhard Theurich <theurich@sourcespring.net>
2025-07-10 22:32:04 +0000

    Switch to /scratch3 for nightly testing on HERA in preparation of removal of the current scratch file system.

--- runscripts/run_esmf_tests_hera.sh
+++ runscripts/run_esmf_tests_hera.sh
@@ -4,12 +4,12 @@ date
 
 hostname -f
 
-export NS=/scratch2/NCEPDEV/stmp1/Gerhard.Theurich/ESMF-Nightly-Testing
+export NS=/scratch3/NCEPDEV/stmp/Gerhard.Theurich/ESMF-Nightly-Testing-Hera
 cd $NS/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
 module load intelpython/2021.3.0
-python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m hera -r $NS >& $NS/hera.log 
+python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m hera -r $NS >& $NS/hera.log
 
 date
 
