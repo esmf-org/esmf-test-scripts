690b50bce0ef643200365aca7e0fb2c059421c77
Bill Sacks <sacks@ucar.edu>
2025-08-15 14:11:29 -0600

    fairy: stop using AppleScript (osascript)
    This osascript-based approach didn't have any performance benefits; I
    guess we need the "tell application \"Terminal\"" piece that we used on
    hearhear, but that doesn't work on fairy.
    
    So instead preparing this for a launchd-based approach.

--- runscripts/run_esmf_tests_fairy.sh
+++ runscripts/run_esmf_tests_fairy.sh
@@ -24,9 +24,8 @@ module() {
 }
 export -f module
 
-# Wrap the python script in a call from an AppleScript to get the cron job to run faster
-# (From https://stackoverflow.com/questions/74793041/why-does-selenium-python-script-run-really-slow-in-cron-compared-to-vscode)
-# (Without a GUI attached, we can't use the "tell application \"Terminal\"" approach from
-# that post, but we can still execute a shell script directly via osascript.)
-osascript -e "do shell script \"cd ${esmftestroot} && python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m fairy -r ${esmftestroot} >& ${esmftestroot}/test_esmf_fairy.log\""
+# Note that we do not run this in the background, because launchd jobs seem to have some
+# problems when running the job in the background.
+cd ${esmftestroot}
+python3 ./esmf-test-scripts/python_scripts/test_esmf.py -m fairy -r ${esmftestroot} >& ${esmftestroot}/test_esmf_fairy.log
 
