d10e34cddcbfa7772dcb49daf5185a83ffb13263
Gerhard Theurich <theurich@sourcespring.net>
2026-04-21 20:45:49 +0000

    Move esmf-test-scripts directory to no-scrub location on Hera.

--- runscripts/run_esmf_tests_hera.sh
+++ runscripts/run_esmf_tests_hera.sh
@@ -5,11 +5,13 @@ date
 hostname -f
 
 export NS=/scratch3/NCEPDEV/stmp/Gerhard.Theurich/ESMF-Nightly-Testing-Hera
-cd $NS/esmf-test-scripts
+# Put esmf-test-scripts in a non-scrubbed location:
+export WORKSPACE=/scratch3/NCEPDEV/nems/Gerhard.Theurich/ESMF-Nightly-Testing-Hera
+cd $WORKSPACE/esmf-test-scripts
 git remote update
 git pull -X theirs --no-edit 
 module load intelpython/2021.3.0
-python3 $NS/esmf-test-scripts/python_scripts/test_esmf.py -m hera -r $NS >& $NS/hera.log
+python3 $WORKSPACE/esmf-test-scripts/python_scripts/test_esmf.py -m hera -r $NS -s $WORKSPACE >& $NS/hera.log
 
 date
 
