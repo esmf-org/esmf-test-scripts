e4ef90faf23d72207bc5d19f53aa9663e3d3b8da
Matthew Thompson <matthew.thompson@nasa.gov>
2026-07-09 14:02:46 -0400

    Fix alderaan runscript

--- runscripts/run_esmf_tests_alderaan.sh
+++ runscripts/run_esmf_tests_alderaan.sh
@@ -1,20 +1,47 @@
 #!/bin/bash -l
 
+set -o pipefail
+
 ESMF_TEST_ROOT=/Users/mathomp4/ESMF_Testing
 LOGDIR=$ESMF_TEST_ROOT/LogFiles
 
-# These are for SLES15
-module use /Users/mathomp4/modulefiles/Core
-module load GEOSpyD
+# Match the existing cron schedule:
+#     00 22 */2 * *
+#
+# Run only on even-numbered days of the month.
+DAY_OF_MONTH=$(date +%d)
+if (( 10#$DAY_OF_MONTH % 2 != 0 )); then
+    exit 0
+fi
 
-cd $ESMF_TEST_ROOT/esmf-test-scripts
+mkdir -p "$LOGDIR"
 
-git remote update
-git pull -X theirs origin --no-edit
+LOGFILE="$LOGDIR/esmf_test_log_$(date +%Y%m%d_%H%M%S).txt"
 
-# We want to tee the logfiles into a file and put the date in the filename so we can keep track of when the tests were run.  The logfiles will be in $LOGDIR
+{
+    echo "ESMF nightly tests started: $(date)"
+    echo "Host: $(hostname)"
+    echo
 
-LOGFILE=$LOGDIR/esmf_test_log_$(date +%Y%m%d_%H%M%S).txt
+    module use /Users/mathomp4/modulefiles/Core
+    module load GEOSpyD
 
-$ESMF_TEST_ROOT/esmf-test-scripts/python_scripts/test_esmf.py -r $ESMF_TEST_ROOT -m alderaan --throttle 1 >& $LOGFILE
+    cd "$ESMF_TEST_ROOT/esmf-test-scripts" || exit 1
+
+    git remote update
+    git pull -X theirs origin --no-edit
+
+    "$ESMF_TEST_ROOT/esmf-test-scripts/python_scripts/test_esmf.py" \
+        -r "$ESMF_TEST_ROOT" \
+        -m alderaan \
+        --throttle 1
+
+    status=$?
+
+    echo
+    echo "ESMF nightly tests finished: $(date)"
+    echo "Exit status: $status"
+
+    exit "$status"
+} >"$LOGFILE" 2>&1
 
